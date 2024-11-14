a8f95f44da36db4f2f3c38f064001d0ac568216e
olegk <kbryzhenyukpc@gmail.com>
2024-11-14 23:15:51 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace reserved;
+
+   internal class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("ваше шестизначное число");
+        string input = Console.ReadLine();
+
+        if (input.Length == 6)
+        {
+            string reversed = "";
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversed += input[i];
+            }
+
+            Console.WriteLine("Число наизнанку" + reversed);
+        }
+        else
+        {
+            Console.WriteLine("Error404");
+        }
+    }
+}
